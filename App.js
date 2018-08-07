1f8a08c7856c46d31813c0586d8808fead9db5ae
imanfitri <imanfitri@users.noreply.github.com>
2018-08-07 12:47:02 +0800

    This one is testing.

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Iman should not shake anything though</Text>
       </View>
     );
   }
