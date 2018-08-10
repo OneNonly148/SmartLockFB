31f0fc18816167861d41b76c446f4b209eb9d2d7
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 11:59:18 +0800

    Altering export class Main

--- Main.js
+++ Main.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
 
-export default class Main extends React.Component {
+export class Main extends React.Component {
   state = { currentUser: null }
 
 render() {
