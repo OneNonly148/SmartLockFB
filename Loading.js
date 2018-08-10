57ab067d1f5cf5b8121821936d8ae43f84eec4c4
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 12:19:27 +0800

    Loading to authentication.

--- Loading.js
+++ Loading.js
@@ -1,18 +1,26 @@
-// This will be the loading screen
+// Loading Screen
 
 import React from 'react'
-import {View, Text, ActivityIndicator, StyleSheet} from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'react-native-firebase'
 
 export default class Loading extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Loading</Text>
-        <ActivityIndicator size='large'/>
+        <ActivityIndicator size="large" />
       </View>
     )
   }
 }
+
+export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
 
 const styles = StyleSheet.create({
   container: {
