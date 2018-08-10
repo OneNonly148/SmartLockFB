3306f4e9d99338187c20cf710b1907a69271293f
Iman Fitri <imanfitri@users.noreply.github.com>
2018-08-10 12:29:34 +0800

    Wiring up handleSignUp method.

--- SignUp.js
+++ SignUp.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
 
 export default class SignUp extends React.Component {
   state = { email: '', password: '', errorMessage: null }
@@ -9,6 +10,12 @@ export default class SignUp extends React.Component {
 handleSignUp = () => {
   // TODO: Firebase stuff...
   console.log('handleSignUp')
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then(() => this.props.navigation.navigate('Main'))
+    .catch(error => this.setState({ errorMessage: error.message }))
+}
 }
 
 render() {
